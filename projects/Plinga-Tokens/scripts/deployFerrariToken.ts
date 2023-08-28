c8470390a538b028b068ae367d2dcad0ac2fa5ef
git <eae966@gmail.com>
2023-08-29 00:18:00 +0330

    Some Updates.

---
+++ projects/Plinga-Tokens/scripts/deployFerrariToken.ts
@@ -0,0 +1,24 @@
+import { ethers, network } from 'hardhat'
+import fs from 'fs'
+
+async function main() {
+    const [owner] = await ethers.getSigners()
+    console.log('owner', owner.address)
+    const FerrariToken = await ethers.getContractFactory("FerrariToken");
+    console.log("Deploying FerrariToken...");
+    const ferrariToken = await FerrariToken.deploy();
+    await ferrariToken.deployed();
+    console.log("FerrariToken deployed to:", ferrariToken.address);
+    const amountInEther = "2000000000";
+    const amountInWei = ethers.utils.parseEther(amountInEther);
+    await ferrariToken.mint(owner.address, amountInWei);
+    console.log("%s token minted to acount %s FerrariToken", amountInEther, owner.address);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
