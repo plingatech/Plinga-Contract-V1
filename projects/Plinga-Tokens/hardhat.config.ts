b304ae85da026a8c9099cb0f770e75da4dcb13e9
git <eae966@gmail.com>
2023-08-29 00:02:28 +0330

    .env.example has been added.

--- projects/Plinga-Tokens/hardhat.config.ts
+++ projects/Plinga-Tokens/hardhat.config.ts
@@ -1,8 +1,159 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import type { HardhatUserConfig, NetworkUserConfig } from 'hardhat/types'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import '@openzeppelin/hardhat-upgrades'
+import '@typechain/hardhat'
+import 'hardhat-watcher'
+import 'dotenv/config'
+import 'solidity-docgen'
+require('dotenv').config({ path: require('find-config')('.env') })
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+const LOW_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.8.19',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: true,
+      runs: 2_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
 
-export default config;
+const LOWEST_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.8.19',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: true,
+      runs: 1_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const NO_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.8.19',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: false,
+      // runs: 1_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const DEFAULT_COMPILER_SETTINGS = {
+  version: '0.8.19',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: true,
+      runs: 1_000_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const bscTestnet: NetworkUserConfig = {
+  url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+  chainId: 97,
+  accounts: [process.env.KEY_TESTNET!],
+}
+
+const bscMainnet: NetworkUserConfig = {
+  url: 'https://bsc-dataseed.binance.org/',
+  chainId: 56,
+  accounts: [process.env.KEY_MAINNET!],
+}
+
+const goerli: NetworkUserConfig = {
+  url: 'https://rpc.ankr.com/eth_goerli',
+  chainId: 5,
+  accounts: [process.env.KEY_GOERLI!],
+}
+
+const eth: NetworkUserConfig = {
+  url: 'https://eth.llamarpc.com',
+  chainId: 1,
+  accounts: [process.env.KEY_ETH!],
+}
+
+const nexichain: NetworkUserConfig = {
+  url: "https://rpc.chainv1.nexi.technology",
+  chainId: 4242,
+  accounts: [process.env.KEY_NEXI!],
+}
+
+const plgchain: NetworkUserConfig = {
+  url: "https://rpcurl.mainnet.plgchain.com",
+  chainId: 242,
+  accounts: [process.env.KEY_PLINGA!],
+}
+
+export default {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    ...(process.env.KEY_TESTNET && { bscTestnet }),
+    ...(process.env.KEY_MAINNET && { bscMainnet }),
+    ...(process.env.KEY_GOERLI && { goerli }),
+    ...(process.env.KEY_ETH && { eth }),
+    ...(process.env.KEY_NEXI && { nexichain }),
+    ...(process.env.KEY_PLINGA && { plgchain }),
+    // mainnet: bscMainnet,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || '',
+    customChains: [
+      {
+        network: "plgchain",
+        chainId: 242,
+        urls: {
+          apiURL: "http://185.128.137.241:4000/api",
+          browserURL: "http://185.128.137.241:4000",
+        },
+      },
+      {
+        network: "nexichain",
+        chainId: 4242,
+        urls: {
+          apiURL: "http://185.173.129.242:4000/api",
+          browserURL: "http://185.173.129.242:4000",
+        },
+      },
+    ]
+  },
+  solidity: {
+    compilers: [NO_OPTIMIZER_COMPILER_SETTINGS],
+    overrides: {
+      'contracts/NonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/test/MockTimeNonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/test/NFTDescriptorTest.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/NFTDescriptorEx.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/NonfungibleTokenPositionDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/libraries/NFTDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+    },
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
+      files: ['./test/**/*'],
+      verbose: true,
+    },
+  },
+  docgen: {
+    pages: 'files',
+  },
+}
